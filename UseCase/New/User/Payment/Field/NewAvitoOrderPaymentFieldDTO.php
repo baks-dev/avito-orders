630d76d4e57e620fb82c0d82dcd909c01146c692
MichelAngelo <baksdevelopmen@gmail.com>
2026-07-05 14:25:32 +0300

    BaksDevelopment

--- UseCase/New/User/Payment/Field/NewAvitoOrderPaymentFieldDTO.php
+++ UseCase/New/User/Payment/Field/NewAvitoOrderPaymentFieldDTO.php
@@ -29,6 +29,7 @@ use BaksDev\Orders\Order\Entity\User\Payment\Field\OrderPaymentFieldInterface;
 use BaksDev\Payment\Type\Field\PaymentFieldUid;
 use Symfony\Component\Validator\Constraints as Assert;
 
+/** @see OrderPaymentField */
 final class NewAvitoOrderPaymentFieldDTO implements OrderPaymentFieldInterface
 {
     /** Идентификатор пользовательского поля в способе оплаты */
@@ -36,7 +37,6 @@ final class NewAvitoOrderPaymentFieldDTO implements OrderPaymentFieldInterface
     private PaymentFieldUid $field;
 
     /** Заполненное значение */
-    #[Assert\Valid]
     private string $value;
 
 
